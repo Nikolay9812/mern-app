ba8d49c18f363f6597662145168409bf23202957
Nikolay9812 <73443601+Nikolay9812@users.noreply.github.com>
2024-04-01 00:15:32 +0200

    create signup api routes

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import userRoutes from './routes/user.routes.js'
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config()
 
@@ -11,8 +12,11 @@ mongoose.connect(process.env.MONGO)
 
 const app = express()
 
+app.use(express.json()) //This will allow us to use json in POSTMAN!!!
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
