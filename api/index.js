e9d8225d48fdcd4391b857100b80b54db37b9872
Nikolay9812 <73443601+Nikolay9812@users.noreply.github.com>
2024-04-07 23:56:03 +0200

    add update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO)
 const app = express()
 
 app.use(express.json()) //This will allow us to use json in POSTMAN!!!
+app.use(cookieParser())
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
